81c6a684efb0e8194700d9a8261e9cfe1efd1b1c
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2022-02-06 15:57:25 -0300

    refactor(components/NotFound): Refatora o componente de pagina nao encontrada

--- dragons/src/components/NotFound.jsx
+++ dragons/src/components/NotFound.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import '../styles/NotFound.css';
+import signIn from '../images/login.png';
 
 export default function NotFound() {
-  return <p>Você precisa estar logado para ver os dragoes!</p>;
+  const { push } = useHistory();
+  function login() {
+    push('/');
+  }
+  return (
+    <div className="notFoundBox">
+      <img src={signIn} className="signIn" alt="you must be logged in" />
+      <p className="mustBeLoggedIn">
+        Você precisa estar logado para ver os dragoes!
+      </p>
+      <button className="buttonLogin" onClick={login}>
+        Fazer Login
+      </button>
+    </div>
+  );
 }
